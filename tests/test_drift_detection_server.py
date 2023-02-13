3d59ce3266115a26a2e00b8612cc4032d79c7fb1
Dawid Smoleń <smolendawid@gmail.com>
2023-02-13 18:47:37 +0100

    fix: initializing DriftDetectionServer in tests

--- tests/test_drift_detection_server.py
+++ tests/test_drift_detection_server.py
@@ -1,14 +1,19 @@
 """Test DriftDetectionServer related functionality."""
 import pytest
+from mlserver.settings import ModelSettings
+
+from drifting.drift_detection_server.server import DriftDetectionServer
 
 
 @pytest.mark.asyncio
-async def test_model_name_exists_error():
+async def test_model_name_exists_error(model_settings: ModelSettings):
     """Test ModelNameExists error."""
+    server = DriftDetectionServer(model_settings)
     assert True
 
 
 @pytest.mark.asyncio
-async def test_fit():
+async def test_fit(model_settings: ModelSettings):
     """Test the fit method."""
+    server = DriftDetectionServer(model_settings)
     assert True
