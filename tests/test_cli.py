ac7fc7dd2bf35665922eeed7c7cfd587d538d677
Dawid Smoleń <smolendawid@gmail.com>
2023-02-13 17:30:08 +0100

    fix: cli test path

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,6 @@
 """Test cli-related utils."""
+import tempfile
+
 import pytest
 
 from drifting.cli.cli import load_all_settings
@@ -7,6 +9,8 @@ from drifting.cli.cli import load_all_settings
 @pytest.mark.asyncio
 async def test_load_server():
     """Point to a folder with no models and create only DriftDetectionServer."""
-    _, models_settings = await load_all_settings("./")
+
+    with tempfile.TemporaryDirectory() as tmp_dirname:
+        _, models_settings = await load_all_settings(tmp_dirname)
 
     assert len(models_settings) == 1
