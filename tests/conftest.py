c9336450c71b9c67d79d7a4cb63041804be5d188
Dawid Smoleń <smolendawid@gmail.com>
2023-02-13 18:49:03 +0100

    fix: merge fixtures with conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,13 @@ from drifting.utils import PACKAGE_ROOT
 TESTS_PATH = os.path.dirname(__file__)
 TESTDATA_PATH = os.path.join(TESTS_PATH, "testdata")
 
+
 @pytest.fixture
 def model_settings() -> ModelSettings:
     """Mock the settings for MLModel."""
-    settings_path = os.path.join(PACKAGE_ROOT, "drift_detection_server", "model-settings.json")
+    settings_path = os.path.join(
+        PACKAGE_ROOT, "drift_detection_server", "model-settings.json"
+    )
     settings = ModelSettings.parse_file(settings_path)
 
     return settings
